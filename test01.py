79899e5ee837264c8e075d47cb8d8e0db4f1a189
Alex <z3381195@126.com>
2024-10-06 22:03:48 +0800

    提交mac创建内容

--- test01.py
+++ test01.py
@@ -1,4 +1,15 @@
-print('123')
-
 import os
 
+#返回系统相关信息
+print(os.name)
+#返回路径分隔符
+print(os.sep)
+#返回当前平台使用的行终止符
+print(repr(os.linesep))
+#返回当前工作目录
+print(os.getcwd())
+#改变当前工作目录
+# os.chdir(r'/Users/zhengzhen/Desktop/pythonTest/a123')
+#当前工作路径下创建和删除文件夹
+os.mkdir('test')
+os.rmdir('test')
